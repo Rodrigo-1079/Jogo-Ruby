e65596c04f36204a6971f66e30501c2cbc4e542e
Rodrigo Araujo <acltm@live.com>
2020-11-26 17:14:57 -0300

    Update advinhacao.rb

--- advinhacao.rb
+++ advinhacao.rb
@@ -1,8 +1,41 @@
-puts 'Bem vindo ao jogo da advinhação'
-puts 'Qual é o seu nome?'
+puts '-' * 20
+puts 'Jogo da advinhação'
+puts '-' * 20
+puts 'Qual é seu nome?'
+puts "\n"
 nome = gets
-puts '\n * 5'
-puts 'Começaremos o jogo para você ' + nome
-puts 'Escolhendo um numero secreto'
-numero_secreto = rand(1..200)
-puts numero_secreto
+puts 'Seja bem vindo jogador ' + nome
+puts "\n" * 3
+puts "Eu escolherei um numero #{nome} e voce tentará advinhar, ok?"
+puts "\n" * 3
+puts "Digite '1' para continuar ou '2' para sair:"
+escolha = gets.to_i
+
+if escolha == 1
+  puts 'Ok estou escolhendo um número entre 0 e 200'   
+else
+  puts 'Até a próxima ' + nome
+  exit
+end
+
+numero = rand(0..200)
+#puts numero
+
+puts 'Tente advinhar minha escolha:'
+chute = gets.to_i
+tentativas = 1
+
+while numero != chute
+    if numero == chute
+        puts "Você acertou meu número é #{numero}"
+        break
+    elsif numero > chute
+        puts "Meu número é maior que #{chute}!!!"
+    else
+        puts "Meu número é menor que #{chute}!!!"
+    end
+    tentativas += 1
+    puts 'Tente novamente'
+    chute = gets.to_i
+end
+puts "Parabens voce acertou em #{tentativas} tentativas"
